0a92e664a43843de9da412fd2988bc88aaabc263
axel9kislin <axel.kislin@hotmail.com>
2016-04-12 18:19:37 +0600

    Вывел данные из бд, разобрался с запросом, создал и заполнил бд, попутно освоил дебаг инстурменты, логи

--- app/src/main/java/com/axel/renotes/MainActivity.java
+++ app/src/main/java/com/axel/renotes/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,9 +19,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MyLogs";
     private List<NoteItem> noteList;
     private RecyclerView mRecyclerView;
     private MyRecyclerAdapter adapter;
+    MyDataBaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +38,33 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyRecyclerAdapter(MainActivity.this, noteList);
         mRecyclerView.setAdapter(adapter);
 
+        helper = new MyDataBaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
         try
         {
-            //создали хелпер, получили базу
-            SQLiteOpenHelper helper = new MyDataBaseHelper(this);
-            SQLiteDatabase db = helper.getReadableDatabase();
-
+            Log.d(TAG, "we in try, after acsess");
             //получаем данные в курсор, всё из базы, для того чтоб потом это вывести в наш recyclerView
-            Cursor cursor = db.query("NOTES", new String[] {"NAME","DESCRIPTION","IMAGE_RESOURCE_ID"},null,null,null,null,null);
-
-            if (cursor.moveToFirst())
-                if (cursor.moveToNext()) {
+            Cursor cursor = db.query("NOTES",
+                    null, //здесь я вставлю норм условие
+                    null,null,null,null,null);
+            int tempIndex = cursor.getColumnIndex("NAME");
+            Log.d(TAG,"we have index " + tempIndex);
+            Log.d(TAG,"we have in cursor "+cursor.getCount());
+            if (cursor.moveToFirst()) {
+                Log.d(TAG,"we can do MoveToFirst!");
+                do {
                     {
-                        String nameText = cursor.getString(0);
-                        String descText = cursor.getString(1);
-                        int img_res = cursor.getInt(2);
+                        String nameText = cursor.getString(1);
+                        String descText = cursor.getString(2);
+                        int img_res = cursor.getInt(3);
+                        Log.d(TAG, "we have name" + nameText);
+                        Log.d(TAG, "we have desc" + descText);
+                        Log.d(TAG, "we have img" + img_res);
 
                         TextView textTitle = (TextView) findViewById(R.id.titleNote);
                         textTitle.setText(nameText);
+                        Log.d(TAG, "we have text" + textTitle.getText() + " in textTitle");
 
                         TextView textDesc = (TextView) findViewById(R.id.disc);
                         textDesc.setText(descText);
@@ -64,12 +76,16 @@ public class MainActivity extends AppCompatActivity {
                         //получили в переменные, теперь нам бы закинуть это в адаптер для ресайкла, или как-то передать в общем на
                         //отрисовку интерфейсу
                     }
-                }
+                }  while (cursor.moveToNext());
+            }else
+            {
+            Log.d(TAG, "We cant moveToFirst()");
+            }
             cursor.close();
             db.close();
         }catch (SQLiteException e)
         {
-            Toast toast = Toast.makeText(this, "Database in unavailable", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
             toast.show();
         }
 
--- app/src/main/java/com/axel/renotes/MyDataBaseHelper.java
+++ app/src/main/java/com/axel/renotes/MyDataBaseHelper.java
@@ -3,14 +3,18 @@ package com.axel.renotes;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.SQLException;
 
 /**
  * Created by Александр on 11.04.2016.
  */
 public class MyDataBaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME ="notesbase";
+    private static final String DB_NAME ="notesbase.db";
     private static final int DB_VERSION = 1;
 
     public MyDataBaseHelper(Context context)
@@ -21,23 +25,40 @@ public class MyDataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE NOTES (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "NAME TEXT" +
-                "DESCRIPTION TEXT" +
+                "NAME TEXT, " +
+                "DESCRIPTION TEXT, " +
                 "IMAGE_RESOURCE_ID INTEGER);");
-        insertNote(db, "Tested note","i simple tested my db", R.drawable.placeholder);
+        Log.d("MyLogs", "it is readOnly?" + db.isReadOnly());
+        insertNote(db, "Tested", "i simple tested my db", R.drawable.placeholder);
+        insertNote(db, "Tested2", "i want do add 2 writes", R.drawable.placeholder);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        onCreate(db);
     }
 
-    public void insertNote(SQLiteDatabase db, String name, String desc, int res_id)
+    public static void insertNote(SQLiteDatabase db, String name, String desc, int res_id)
     {
+
         ContentValues cv = new ContentValues();
         cv.put("NAME", name);
+        Log.d("MyLogs", "we have in cv.name is " + cv.get("NAME") + " string");
         cv.put("DESCRIPTION", desc);
+        Log.d("MyLogs", "we have in cv.desc is " + cv.get("DESCRIPTION") + " string");
         cv.put("IMAGE_RESOURCE_ID", res_id);
-        db.insert("NOTES",null,cv);
+        Log.d("MyLogs", "we have in cv.image_resources is " + cv.get("IMAGE_RESOURCE_ID") + " string");
+        try
+        {
+            long rowID = db. insertOrThrow("NOTES", null, cv);
+            Log.d("MyLogs","we added in db "+rowID +" row");
+        }
+        catch (SQLiteException e)
+        {
+            Log.d("MyLogs", e.getMessage());
+        }
+
+
     }
 }
